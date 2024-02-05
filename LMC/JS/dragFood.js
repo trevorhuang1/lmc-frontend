4615ddb7741e36c1476187c29eea4ed100cbdb09
M8tth3 <mw.mountaindragon@gmail.com>
2024-02-05 08:28:54 -0800

    all drop zones

--- LMC/JS/dragFood.js
+++ LMC/JS/dragFood.js
@@ -1,28 +1,23 @@
 const card = document.getElementById("card");
-const dropZone = document.getElementById("drop-zone-UL");
+const dropZoneUL = document.getElementById("drop-zone-UL");
 const dropZoneUR = document.getElementById("drop-zone-UR");
-
+const dropZoneBL = document.getElementById("drop-zone-BL");
+const dropZoneBR = document.getElementById("drop-zone-BR");
+const dropZones = [dropZoneUL,dropZoneUR,dropZoneBL,dropZoneBR];
 card.addEventListener('dragstart', function(event) {
     console.log(event);
 })
 
-dropZone.addEventListener('dragover', function(event)
+dropZones.forEach(function(dropZone)
 {
-    event.preventDefault();
-})
-dropZone.addEventListener('drop', function(event)
-{
-    dropZone.prepend(card);
-    card.style.width = "300px";
-    card.style.height = "250px";
-})
-dropZoneUR.addEventListener('dragover', function(event)
-{
-    event.preventDefault();
-})
-dropZoneUR.addEventListener('drop', function(event)
-{
-    dropZoneUR.prepend(card);
-    card.style.width = "300px";
-    card.style.height = "250px";
+    dropZone.addEventListener('dragover', function(event)
+    {
+        event.preventDefault();
+    })
+    dropZone.addEventListener('drop', function(event)
+    {
+        dropZone.prepend(card);
+        card.style.width = "300px";
+        card.style.height = "250px";
+    })
 })
