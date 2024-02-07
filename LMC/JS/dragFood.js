f5cec2e39df3b4224e0f4a27dc9bb5bab7bf6071
M8tth3 <mw.mountaindragon@gmail.com>
2024-02-07 11:52:16 -0800

    sidenav

--- LMC/JS/dragFood.js
+++ LMC/JS/dragFood.js
@@ -1,8 +1,10 @@
 const card = document.getElementById("card");
+var counter = 0;
 const dropZoneUL = document.getElementById("drop-zone-UL");
 const dropZoneUR = document.getElementById("drop-zone-UR");
 const dropZoneBL = document.getElementById("drop-zone-BL");
 const dropZoneBR = document.getElementById("drop-zone-BR");
+
 const dropZones = [dropZoneUL,dropZoneUR,dropZoneBL,dropZoneBR];
 card.addEventListener('dragstart', function(event) {
     console.log(event);
@@ -21,3 +23,22 @@ dropZones.forEach(function(dropZone)
         card.style.height = "250px";
     })
 })
+
+function openNav(x)
+{
+    console.log(counter);
+    if (counter < 1){
+        document.getElementById("sideNav").style.width = "200px";
+        x.classList.toggle("change");
+        counter += 1;
+    }
+    else
+    {
+       closeNav();
+    }        
+}
+function closeNav()
+{
+    document.getElementById("sideNav").style.width = "50px";
+    counter = 0;
+}
