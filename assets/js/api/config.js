2c23efdb77cda91675d228548e0ba576b7da5f3e
hanlunli <hanlunli.007@gmail.com>
2024-02-25 16:10:32 -0800

    adding links

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,10 +1,10 @@
 export var uri;
 if (location.hostname === "localhost") {
-        uri = "http://localhost:8028";
+        uri = "http://lmcflask.stu.nighthawkcodingsociety.com";
 } else if (location.hostname === "127.0.0.1") {
-        uri = "http://127.0.0.1:8028";
+        uri = "http://lmcflask.stu.nighthawkcodingsociety.com";
 } else {
-        uri = "http://127.0.0.1:8028";
+        uri = "http://lmcflask.stu.nighthawkcodingsociety.com";
 }
 
 export const options = {
