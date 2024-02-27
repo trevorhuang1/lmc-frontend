e1b625d270c65140a80049686d8c8ffe5d19fe61
hanlunli <hanlunli.007@gmail.com>
2024-02-27 10:28:58 -0800

    reverting to localhost

--- _posts/2024-1-23-lmcLogin.md
+++ _posts/2024-1-23-lmcLogin.md
@@ -63,7 +63,7 @@ The script defines a function when the page loads. This function is triggered wh
 <script>
 
     // uri variable and options object are obtained from config.js
-    const uri = "https://lmcflask.stu.nighthawkcodingsociety.com";
+    const uri = "http://127.0.0.1:8028/";
     const options = {
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,10 +1,10 @@
 export var uri;
 if (location.hostname === "localhost") {
-        uri = "https://lmcflask.stu.nighthawkcodingsociety.com";
+        uri = "http://127.0.0.1:8028/";
 } else if (location.hostname === "127.0.0.1") {
-        uri = "https://lmcflask.stu.nighthawkcodingsociety.com";
+        uri = "http://127.0.0.1:8028/";
 } else {
-        uri = "https://lmcflask.stu.nighthawkcodingsociety.com";
+        uri = "http://127.0.0.1:8028/";
 }
 
 export const options = {
